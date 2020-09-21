05c6fb3028139a096f3128dd1620ee656e722b3d
utgwkk <utagawakiki@gmail.com>
2020-09-21 12:27:02 +0000

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::PerlFuzzyTokenFinder - It's new $module
+
+# SYNOPSIS
+
+    use App::PerlFuzzyTokenFinder;
+
+# DESCRIPTION
+
+App::PerlFuzzyTokenFinder is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/App/PerlFuzzyTokenFinder.pm
@@ -0,0 +1,39 @@
+package App::PerlFuzzyTokenFinder;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::PerlFuzzyTokenFinder - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::PerlFuzzyTokenFinder;
+
+=head1 DESCRIPTION
+
+App::PerlFuzzyTokenFinder is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
