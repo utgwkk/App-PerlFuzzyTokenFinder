69888a0d8fed9c39bf49b007de8d09981472b131
utgwkk <utagawakiki@gmail.com>
2020-09-22 12:25:32 +0000

    matched position object

---
+++ lib/App/PerlFuzzyTokenFinder/MatchedPosition.pm
@@ -0,0 +1,40 @@
+package App::PerlFuzzyTokenFinder::MatchedPosition;
+use strict;
+use warnings;
+use Carp qw(croak);
+
+# args:
+#   filename: Str
+#   line_number: Num
+#   statement: PPI::Statement, should be cloned with PPI::Statement#clone
+sub new {
+    my ($class, %args) = @_;
+
+    my $filename = delete $args{filename};
+    croak "filename required" unless defined $filename;
+
+    my $line_number = delete $args{line_number};
+    croak "line_number required" unless defined $line_number;
+
+    my $statement = delete $args{statement};
+    croak "statement required" unless defined $statement;
+
+    bless +{
+        filename    => $filename,
+        line_number => $line_number,
+        statement   => $statement,
+    };
+}
+
+# getter
+sub filename { shift->{filename} }
+sub line_number { shift->{line_number} }
+sub statement { shift->{statement} }
+
+sub format_for_print {
+    my $self = shift;
+
+    sprintf "%s:%d:%s", $self->filename, $self->line_number, $self->statement->content;
+}
+
+1;
