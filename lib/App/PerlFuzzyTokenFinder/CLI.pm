cbddf8061d95fad10de8c2e9ded392d03544e5a6
utgwkk <utagawakiki@gmail.com>
2020-09-23 13:44:16 +0900

    move all core implementation to CLI

--- lib/App/PerlFuzzyTokenFinder/CLI.pm
+++ lib/App/PerlFuzzyTokenFinder/CLI.pm
@@ -2,7 +2,10 @@ package App::PerlFuzzyTokenFinder::CLI;
 use strict;
 use warnings;
 
+use PPI;
+
 use App::PerlFuzzyTokenFinder;
+use App::PerlFuzzyTokenFinder::MatchedPosition;
 
 sub new {
     my ($class) = @_;
@@ -27,6 +30,40 @@ sub set_target_files {
     my ($self, $target_files) = @_;
     
     $self->{target_files} = $target_files;
+}
+
+sub find {
+    my $self = shift;
+    my $find_tokens = $self->find_tokens;
+
+    my $found = 0;
+
+    for my $file (@{$self->target_files}) {
+        my $doc = PPI::Document->new($file);
+        die "Parse $file failed" unless $doc;
+
+        my $stmts = $doc->find('PPI::Statement');
+        next unless $stmts;
+
+        for my $stmt (@$stmts) {
+            my $target_tokens = [ $stmt->children ];
+            if (App::PerlFuzzyTokenFinder->matches($target_tokens, $find_tokens)) {
+                my $matched = App::PerlFuzzyTokenFinder::MatchedPosition->new(
+                    filename    => $file,
+                    line_number => $stmt->line_number,
+                    statement   => $stmt->clone,
+                );
+                print $matched->format_for_print, "\n";
+                $found = 1;
+            }
+        }
+    }
+
+    if ($found) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 1;
